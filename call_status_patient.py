c54f0555641364d7894810a4db4acafe24d13002
Claire Niederriter <you@example.com>
2018-11-14 16:45:19 -0500

    update file

--- call_status_patient.py
+++ call_status_patient.py
@@ -1,9 +1,9 @@
 import requests
 
 def call_status():
-    my_dict = {"user_age": 50, "heart_rate": 100}
+    my_dict = {"user_age": 50, "heart_rate": 120}
     r = requests.get("http://127.0.0.1:5000/api/status/patient", json=my_dict)
-    answer = r.text()
+    answer = r.text
     print(answer)
 
 
