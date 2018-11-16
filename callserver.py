8c10dab830a7400fa4f2f31cbd1e77feebf3edad
Claire Niederriter <you@example.com>
2018-11-16 11:53:09 -0500

    pep8 updates

--- callserver.py
+++ callserver.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def call_server():
     r = requests.post("http://127.0.0.1:5000/api/new_patient")
     answer = r.json()
--- callserverhr.py
+++ callserverhr.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def call_server_hr():
     r = requests.post("http://127.0.0.1:5000/api/heart_rate")
     answer = r.json()
